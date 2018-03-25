6e5c528b26d3211600fddc774eb97d863d0a22e8
thirupathirao015 <ramisetti015@gmail.com>
2018-03-25 19:46:13 +0530

    addition of another values...

--- TestProgram/src/pack/Hello.java
+++ TestProgram/src/pack/Hello.java
@@ -5,8 +5,10 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a=10;
-		int b=20;
+		int b=20,d=50;
 		int c=a+b;
+		System.out.println("addtion of two number"+":"+c);
+		c=c+d;	
 		System.out.println("addtion of two number"+":"+c);
 
 	}
