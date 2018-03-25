1fba72f41245bdfe3d57de0253a9c89d30f67f96
thirupathirao015 <ramisetti015@gmail.com>
2018-03-25 19:38:05 +0530

    My first java class

---
+++ TestProgram/src/pack/Hello.java
@@ -0,0 +1,14 @@
+package pack;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println("addtion of two number"+":"+c);
+
+	}
+
+}
